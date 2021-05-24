899e75aba384edda70d9a51b8b7ec9cee9a7bc3e
Lucas <lucas.marti@dxdt.ch>
2021-05-24 22:42:03 +0200

    finished first working version

--- js/jsexpressiongenerator.js
+++ js/jsexpressiongenerator.js
@@ -1,10 +1,16 @@
 
-var var_names=[]
+var int_vars_reg=[]
 var var_exp="var"
 var status_good_color = "#4DAC27"	
 var status_bad_color = "#DA1400"
 var status_info_color = "#49BEED"
 var var_sym = "$"
+var copied_str = ""
+
+function copy_to_clip(str) {
+	copied_str=str
+	document.execCommand('copy');
+};
 
 function get_random_int(max) {
 	return Math.floor(Math.random() * max);
@@ -18,7 +24,6 @@ function replace_nested_or(str){
 		matches = [...str.matchAll(re)]
 		matches.forEach(function(e){
 			num_commas=e[1].split(",").length
-			console.log(num_commas)
 			let ind = get_random_int(num_commas)
 			choice=e[1].split(",")[ind]
 			str = str.replace(e[0],choice)
@@ -34,15 +39,27 @@ function get_file_from_path(str) {
 }
 
 function get_var_name() {
-	var var_len = var_names.length
+	var var_len = int_vars_reg.length
 	var var_num = var_len
 	var var_name = var_exp + var_num
-	while (var_names.includes(var_name)){
+	while (int_vars_reg.includes(var_name)){
 		var_num++
 		var_name = var_exp + var_num
 	}
-	var_names.push(var_name)
+	int_vars_reg.push(var_name)
 	return var_name
+}
+
+function cap_first(str) {
+	return str[0].toUpperCase() + str.substring(1) 
+}
+function check_case(char){
+	if (char.toUpperCase() == char) {
+		return true;
+	}
+	else {
+		return false
+	}
 }
 
 function add_var_div(){
@@ -81,18 +98,49 @@ function add_var_div(){
 	return var_div
 }
 
+function strip_html(str){
+	return str.replace(/(<([^>]+)>)/gi, "");
+}
+
+function ajax_wl(wl_fname){
+	let wl_data = []
+	let url_fpath = source_path_active + "wordlists/" + wl_fname
+	
+	$.ajax(
+	{
+		url: url_fpath,
+		dataType: "html",
+		async: false,
+		success: function(data)
+		{
+			
+			wl_data = data;
+		},
+	});	
+	return wl_data.split("\n")
+}
+function get_random_item_from_array(arr){
+	ind = get_random_int(arr.length)
+	return arr[ind]
+}
+
+function get_random_from_wl(wl_fname){
+	return get_random_item_from_array(ajax_wl(wl_fname))
+}
+
 var source_path = "procedural-generation-word-lists/"
 var source_path_gh = "https://raw.githubusercontent.com/lmarti-dev/procedural-generation-word-lists/main/"
+var source_path_active = source_path_gh
 var manifest
+var can_generate=false
 
 $.ajax(
 {
-    url: source_path_gh + "manifest.json",
+    url: source_path_active + "manifest.json",
     dataType: "json",
     async: false,
     success: function(data)
     {
-		
         manifest = data;
     }
 });
@@ -110,26 +158,78 @@ $("#add-var").click(function(){
 	})
 $("#reset-var").click(function(){
 		$("#var-list").html("")
-		var_names=[]
+		int_vars_reg=[]
 	})
+
+
+function get_multiple_inputs(query){
+	let values = $(query).map(function(idx,elem) {
+		//~ clown world implementation of argument order
+		return $(elem).val();
+	})
+	return Array.from(values)
+	
+}
+
+function get_stripped_gen_text(){
+	return strip_html(document.getElementById("gen-text").value)
+}
 
 $("#generate").click(function(){
 	if (can_generate){
-		let varnames = document.querySelectorAll('[id^="input-"]')	
-		let wls = $('[id^="select-"]')
-		let gen_text=document.getElementById("gen-text").value
-		let pairs = wls.map(function(e, i) {
-			return [e, varnames[i]];
-		});
+		$("#results").html("Generating...")
+		$("#results").html("")
 		
+		let varnames = get_multiple_inputs('[id^="input-"]')
+		let wls = get_multiple_inputs('[id^="select-"]')
+		let num_gens=$("#numgens").val()
+		for (iii=0;iii<num_gens;iii++){
+			let text=replace_nested_or(get_stripped_gen_text())
+			varnames.forEach(function(e, i) {
+				
+				let random_word = get_random_from_wl(wls[i])
+				
+				if (check_case(e[0])){
+					text=text.replace(var_sym + e, cap_first(random_word))
+				}
+				else {
+					text=text.replace(var_sym + e, random_word)
+				}
+				
+			});
+			let result = $("<div class='result'></div>")
+			result.html("<p>"+text+"</p>")
+			result.append("<button class='copy'>Copy</button>")
+			$("#results").append(result)
+		}
+		$("#results").on("click",".copy",function(){
+			copy_to_clip(this.parentNode.firstElementChild.innerHTML)
+		})
 	}
 })
 
 
 var app = document.getElementById("expgenapp")
-app.addEventListener('input', update_message);
-app.addEventListener('click', update_message);
+app.addEventListener('input', update_ui);
+app.addEventListener('click', update_ui);
 var can_generate = false
+
+function highlight_vars(e){
+	let text = get_stripped_gen_text()
+	let varnames = get_multiple_inputs('[id^="input-"]')
+	varnames.forEach(function(e, i) {
+		varname=var_sym + e
+		text=text.replace(varname, "<code>" + varname + "</code>")	
+	});
+	document.getElementById("gen-text").innerHTML=text
+	
+}
+
+function update_ui(e){
+	//~ highlight_vars(e)
+	update_message(e)
+}
+
 
 function update_status_message(str,color){
 	let msg = $("<div></div>")
@@ -146,7 +246,7 @@ function update_message(e){
 	var bad_var=false
 	var null_var=false
 	var no_vars=false
-	var can_generate=false
+
 	
 	inputs.forEach(function(e) {
 		if (e.value === "") {
@@ -173,3 +273,9 @@ function update_message(e){
 		can_generate=true;
 	}
 }
+
+document.addEventListener('copy', function(e) {
+	e.clipboardData.setData('text/plain', copied_str);
+	e.clipboardData.setData('text/html', copied_str);
+	e.preventDefault();
+})	

318bd57875d4120b5ce6cbebbf5948dfa3f36f19
lmarti <lucas.marti@fau.de>
2024-03-30 10:19:27 +0100

    changed wordlist source

--- js/jsexpressiongenerator.js
+++ js/jsexpressiongenerator.js
@@ -11,7 +11,7 @@ const json_gen_key = "generative"
 var source_path = "procedural-generation-word-lists/"
 var source_path_wb = "../wordlists/"
 var source_path_gh = "https://raw.githubusercontent.com/lmarti-dev/procedural-generation-word-lists/main/"
-var source_path_active = source_path_gh
+var source_path_active = source_path_wb
 var wl_manifest
 
 //~ UTILS 

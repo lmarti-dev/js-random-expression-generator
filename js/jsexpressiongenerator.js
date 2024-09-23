4a03f861b0f4c18193a350252363291e3001908d
lmarti <lucas.marti@fau.de>
2024-09-23 19:33:51 +0200

    js

--- js/jsexpressiongenerator.js
+++ js/jsexpressiongenerator.js
@@ -1,170 +1,175 @@
+//~ GLOBALS
+const var_sym = "$";
+const or_sym = "|";
 
-//~ GLOBALS 
-const var_sym = "$"
-const or_sym = "|"
+const json_var_key = "vars";
+const json_wl_key = "wordlists";
+const json_gen_key = "generative";
 
-const json_var_key = "vars"
-const json_wl_key = "wordlists"
-const json_gen_key = "generative"
+var source_path = "procedural-generation-word-lists/";
+var source_path_wb = "https://dxdt.ch/projects/wordlists/wordlists";
+var source_path_gh =
+  "https://raw.githubusercontent.com/lmarti-dev/procedural-generation-word-lists/main/";
+var source_path_active = source_path_gh;
+var wl_manifest;
 
-var source_path = "procedural-generation-word-lists/"
-var source_path_wb = "https://dxdt.ch/projects/wordlists/wordlists"
-var source_path_gh = "https://raw.githubusercontent.com/lmarti-dev/procedural-generation-word-lists/main/"
-var source_path_active = source_path_gh
-var wl_manifest
-
-//~ UTILS 
-
+//~ UTILS
 
 async function fetch_json(url) {
-	return await (await fetch(url, {
-		method: "GET"
-	})).json()
+  return await (
+    await fetch(url, {
+      method: "GET",
+    })
+  ).json();
 }
 
 async function fetch_text(url) {
-	return await (await fetch(url, {
-		method: "GET",
-	})).text()
+  return await (
+    await fetch(url, {
+      method: "GET",
+    })
+  ).text();
 }
-
-
 
 function get_random_int(max) {
-	return Math.floor(Math.random() * max);
+  return Math.floor(Math.random() * max);
 }
 function get_random_item_from_array(arr) {
-	ind = get_random_int(arr.length)
-	return arr[ind]
+  ind = get_random_int(arr.length);
+  return arr[ind];
 }
 
 async function get_random_from_wordlist(wl_fname) {
-	let wordlist = await fetch_wordlist(wl_fname)
-	return get_random_item_from_array(wordlist)
+  let wordlist = await fetch_wordlist(wl_fname);
+  return get_random_item_from_array(wordlist);
 }
 
 function replace_nested_or(str) {
-	let re = /\{([^\{\}]+?)\}/g
-	N = 0
-	matches = [...str.matchAll(re)]
-	while (matches.length !== 0 && N < 1000) {
-		matches = [...str.matchAll(re)]
-		matches.forEach(function (e) {
-			num_commas = e[1].split(or_sym).length
-			let ind = get_random_int(num_commas)
-			choice = e[1].split(or_sym)[ind]
-			str = str.replace(e[0], choice)
-		})
-		N += 1
-	}
-	return str
+  let re = /\{([^\{\}]+?)\}/g;
+  N = 0;
+  matches = [...str.matchAll(re)];
+  while (matches.length !== 0 && N < 1000) {
+    matches = [...str.matchAll(re)];
+    matches.forEach(function (e) {
+      num_commas = e[1].split(or_sym).length;
+      let ind = get_random_int(num_commas);
+      choice = e[1].split(or_sym)[ind];
+      str = str.replace(e[0], choice);
+    });
+    N += 1;
+  }
+  return str;
 }
-
 
 function clean_file_path(str) {
-	str = str.split('\\').pop().split('/').pop();
-	return str.split("\.").shift()
+  str = str.split("\\").pop().split("/").pop();
+  return str.split(".").shift();
 }
-
 
 function cap_first(str) {
-	return str[0].toUpperCase() + str.substring(1)
+  return str[0].toUpperCase() + str.substring(1);
 }
 function is_uppercase(char) {
-	if (char.toUpperCase() == char) {
-		return true;
-	}
-	else {
-		return false
-	}
+  if (char.toUpperCase() == char) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function strip_html(str) {
-	return str.replace(/(<([^>]+)>)/gi, "");
+  return str.replace(/(<([^>]+)>)/gi, "");
 }
 
 async function fetch_wordlist(wl_fname) {
-	var response;
-	let url = `${source_path_active}wordlists/${wl_fname}`
-	text = await fetch_text(url)
-	wordlist = text.split(/\r?\n/)
-	return wordlist
-
+  var response;
+  let url = `${source_path_active}wordlists/${wl_fname}`;
+  text = await fetch_text(url);
+  wordlist = text.split(/\r?\n/);
+  return wordlist;
 }
 
 function get_option_list_from_array(arr) {
-	var opts = []
-	arr.forEach(element => opts.push(`<option value="${element}">${clean_file_path(element)}</option>`));
-	return opts
+  var opts = [];
+  arr.forEach((element) =>
+    opts.push(`<option value="${element}">${clean_file_path(element)}</option>`)
+  );
+  return opts;
 }
 
 //~ GENERATE =========================
 
-
 async function get_random_words(elems) {
-	var random_words = []
-	for (let i = 0; i < elems.length; i++) {
-		w = await get_random_from_wordlist(elems[i])
-		random_words.push(w)
-	}
-	return random_words
-
+  var random_words = [];
+  for (let i = 0; i < elems.length; i++) {
+    w = await get_random_from_wordlist(elems[i]);
+    random_words.push(w);
+  }
+  return random_words;
 }
 
 async function evaluate_generative_expression(varnames, wordlists, gen_text) {
-	let varnames_split = []
-	let wls_split = []
-	for (iii = 0; iii < varnames.length; iii++) {
-		if (varnames[iii].includes(",")) {
-			let vars = varnames[iii].split(",")
-			vars.forEach(function (e, i) {
-				varnames_split.push(e)
-				wls_split.push(wordlists[iii])
-			});
-		}
-		else {
-			varnames_split.push(varnames[iii])
-			wls_split.push(wordlists[iii])
-		}
-	}
-	varnames = varnames_split
-	wordlists = wls_split
+  let varnames_split = [];
+  let wls_split = [];
+  for (iii = 0; iii < varnames.length; iii++) {
+    if (varnames[iii].includes(",")) {
+      let vars = varnames[iii].split(",");
+      vars.forEach(function (e, i) {
+        varnames_split.push(e);
+        wls_split.push(wordlists[iii]);
+      });
+    } else {
+      varnames_split.push(varnames[iii]);
+      wls_split.push(wordlists[iii]);
+    }
+  }
+  varnames = varnames_split;
+  wordlists = wls_split;
 
-	//~ evaluate the longest variables first to avoid replacing longer ones with shorter ones with same beginning
+  //~ evaluate the longest variables first to avoid replacing longer ones with shorter ones with same beginning
 
-	zipped_data = varnames.map(function (e, i) { return [e, wordlists[i]] })
-	zipped_data.sort(function (a, b) { return b[0].length - a[0].length });
+  zipped_data = varnames.map(function (e, i) {
+    return [e, wordlists[i]];
+  });
+  zipped_data.sort(function (a, b) {
+    return b[0].length - a[0].length;
+  });
 
-	let text = replace_nested_or(gen_text)
-	var random_words = await get_random_words(zipped_data.map((e) => { return e[1] }))
-	zipped_data.forEach(function (e, i) {
-		let cur_varname = e[0]
-		let cur_wl = e[1]
-		let random_word = random_words[i]
-		let capped_word;
+  let text = replace_nested_or(gen_text);
+  var random_words = await get_random_words(
+    zipped_data.map((e) => {
+      return e[1];
+    })
+  );
+  zipped_data.forEach(function (e, i) {
+    let cur_varname = e[0];
+    let cur_wl = e[1];
+    let random_word = random_words[i];
+    let capped_word;
 
-		if (is_uppercase(cur_varname[0])) {
-			capped_word = cap_first(random_word)
-		}
-		else {
-			capped_word = random_word
-		}
-		text = text.replaceAll(var_sym + cur_varname, capped_word)
-
-	});
-	return text
+    if (is_uppercase(cur_varname[0])) {
+      capped_word = cap_first(random_word);
+    } else {
+      capped_word = random_word;
+    }
+    text = text.replaceAll(var_sym + cur_varname, capped_word);
+  });
+  return text;
 }
 
 async function evaluate_json(data) {
-	return await evaluate_generative_expression(data[json_var_key], data[json_wl_key], data[json_gen_key])
-
+  return await evaluate_generative_expression(
+    data[json_var_key],
+    data[json_wl_key],
+    data[json_gen_key]
+  );
 }
 
 function set_source_path(new_source_path) {
-	source_path_active = new_source_path
+  source_path_active = new_source_path;
 }
 
 async function load_wordlist_manifest() {
-	//~ LOAD MANIFEST 
-	wl_manifest = await fetch_json(`${source_path_active}manifest.json`)
+  //~ LOAD MANIFEST
+  wl_manifest = await fetch_json(`${source_path_active}manifest.json`);
 }

2943582a513cc5264f7ffc0c09f18f9a1b22741b
lmarti <lucas.marti@fau.de>
2024-09-25 22:20:23 +0200

    added a no repeat option to wordlist generation. You remove each item you pick from the wordlist, so that the next variable will not access it

--- js/jsexpressiongenerator.js
+++ js/jsexpressiongenerator.js
@@ -15,6 +15,11 @@ var wl_manifest;
 
 //~ UTILS
 
+function regex_an(s) {
+  // later..
+  RegExp.replaceAll(/a ([aioe])/, "an $1");
+}
+
 async function fetch_json(url) {
   return await (
     await fetch(url, {
@@ -99,16 +104,50 @@ function get_option_list_from_array(arr) {
 
 //~ GENERATE =========================
 
-async function get_random_words(elems) {
+async function get_random_words(elems, repeat) {
   var random_words = [];
-  for (let i = 0; i < elems.length; i++) {
-    w = await get_random_from_wordlist(elems[i]);
-    random_words.push(w);
+  if (repeat) {
+    for (let i = 0; i < elems.length; i++) {
+      w = await get_random_from_wordlist(elems[i]);
+      random_words.push(w);
+    }
+  } else {
+    // how can js be this bad at simple operations?
+    // this is a two-liner in python:
+    // d = {k:[] for k in set(elems)}
+    // [d[k].append(j) for j,k in zip(range(len(elems)),elems)]
+    // profusely sweating programmer meme face
+
+    random_words = Array(elems.length).fill("null");
+    unique_wls = [...new Set(elems)];
+    obj = Object.fromEntries(unique_wls.map((e) => [e, []]));
+    for (var i = 0; i < elems.length; i++) {
+      obj[[elems[i]]].push(i);
+    }
+    let wordlist;
+    let idx;
+    for (var wl in obj) {
+      wordlist = await fetch_wordlist(wl);
+      for (var ind in obj[[wl]]) {
+        if (wordlist.length == 0) {
+          random_words[obj[[wl]][ind]] = "no words left";
+        } else {
+          idx = get_random_int(wordlist.length);
+          random_words[obj[[wl]][ind]] = wordlist[idx];
+          wordlist.splice(idx, 1);
+        }
+      }
+    }
   }
   return random_words;
 }
 
-async function evaluate_generative_expression(varnames, wordlists, gen_text) {
+async function evaluate_generative_expression(
+  varnames,
+  wordlists,
+  gen_text,
+  repeat = true
+) {
   let varnames_split = [];
   let wls_split = [];
   for (iii = 0; iii < varnames.length; iii++) {
@@ -139,7 +178,7 @@ async function evaluate_generative_expression(varnames, wordlists, gen_text) {
   var random_words = await get_random_words(
     zipped_data.map((e) => {
       return e[1];
-    })
+    }, repeat)
   );
   zipped_data.forEach(function (e, i) {
     let cur_varname = e[0];
@@ -157,11 +196,12 @@ async function evaluate_generative_expression(varnames, wordlists, gen_text) {
   return text;
 }
 
-async function evaluate_json(data) {
+async function evaluate_json(data, repeat = true) {
   return await evaluate_generative_expression(
     data[json_var_key],
     data[json_wl_key],
-    data[json_gen_key]
+    data[json_gen_key],
+    repeat
   );
 }
 
